eebb423e740c0d11682b75997c6ce9fea4d757ad
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-18 11:55:50 +0800

    added create table with go and edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,8 +10,11 @@ class HomeController extends Controller
     //
     public function index()
     {
-
+        /*
         $title = 'Fumetti';
         return view('home', compact('title'));
+        */
+        $comics = Comic::all();
+        return view('home', compact('comics'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,5 +7,39 @@
 
 @section('content')
 
+    
     <h1 class="text-center fw-bold"><a href="{{ route('comics.index') }}">COLLECTION COMICS</a></h1>  
+
+
+        <table class="table table-dark table-striped">
+            <h2 class="text-center fw-bold my-5 text-primary">Elenco Fumetti</h2>
+            <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Series</th>
+                <th scope="col">Type</th>
+                <th scope="col">Price</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                @forelse ($comics as $comic)
+                <tr>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>{{$comic->price}}</td>
+                    
+                    <td><a href="{{route('comics.show', $comic->id)}}" class="btn btn-warning me-2">Go</a>
+                    <a href="{{route('comics.edit', $comic->id)}}" class="btn btn-danger">Edit</a></td>
+                    
+                </tr>
+                @empty
+                    <td colspan="5" class="text-center">Nessun risultato corrispondente</td>
+                @endforelse
+           
+            </tbody>
+        </table>
+
+
 @endsection 
